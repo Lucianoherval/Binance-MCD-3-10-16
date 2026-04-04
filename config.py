e0d841bada976ec8ef36e3beceda0315c9d10e76
Luciano Herval <lucianoherval@gmail.com>
2026-04-04 15:45:32 -0300

    Bot Trader Finalizado e operando!

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 BINANCE_API_KEY = os.getenv('BINANCE_API_KEY')
 BINANCE_API_SECRET = os.getenv('BINANCE_API_SECRET')
 PAR = 'BTC/BRL'
-INTERVALO = '1h'
+INTERVALO = '5m'
 CSV_PATH = 'dados/operacoes.csv'
